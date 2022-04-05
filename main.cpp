bf55fcfc2051f0db02ab60bb4de67a588bcaa656
ZiXuan_Xia <1321760986@qq.com>
2022-04-05 23:47:31 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,20 +1,107 @@
+#include <vector>
+#include <cmath>
 #include "TgaImage.h"
+#include "Model.h"
+#include "geometry.h"
+
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image,TGAColor color) {
-	for (float t = 0.; t < 1.; t += .01) {
-		int x = x0 + (x1 - x0) * t;
-		int y = y0 + (y1 - y0) * t;
-		image.set(x, y, color);
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+	// Solid line(all)
+	//for (float t = 0.; t < 1.; t += .01) {
+	//	int x = x0 + (x1 - x0) * t;
+	//	int y = y0 + (y1 - y0) * t;
+	//	image.set(x, y, color);
+	//}
+
+	//Point line(only red or white)
+	//for (int x = x0; x <= x1; x++) {
+	//	float t = (x - x0) / (float)(x1 - x0);
+	//	int y = y0 * (1. - t) + y1 * t;
+	//	image.set(x, y, color);
+	//}
+
+
+	// Dotted line(Point line)
+	//for (float t = 0.; t < 1.; t += .1) {
+	//	int x = x0 * (1. - t) + x1 * t;
+	//	int y = y0 * (1. - t) + y1 * t;
+	//	image.set(x, y, color);
+	//}
+
+	//Whole line
+	bool steep = false;
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {//if the line is steep,we transpose the image
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	if (x0 > x1) { //make it left-to-right
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	//int dx = x1 - x0;
+	//int dy = y1 - y0;
+	
+	//float derror = std::abs(dy / float(dx));
+	//float error = 0;
+
+	//int derror2 = std::abs(dy) * 2;
+	//int error2 = 0;
+	//int y = y0;
+
+	for (int x = x0; x <= x1; x++) {
+		float t = (x - x0) / (float)(x1 - x0);
+		int y = y0 * (1. - t) + y1 * t;
+		if (steep) {
+			image.set(y, x, color);//if transposed,de-transpose
+		}
+		else {
+			image.set(x, y, color);
+		}
+		//error2 += derror2;
+		//if (error2 > dx) {
+		//	y += (y1 > y0 ? 1 : -1);
+		//	error2 -= dx * 2;
+		//}
 	}
 }
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	line(13, 20, 80, 40, image, white);
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	}else {
+		model = new Model("obj/african_head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
+
+	//for (int i = 0; i < 1000000; i++) {
+	//	line(13, 20, 80, 40, image, white);
+	//	line(20, 13, 40, 80, image, red);
+	//	line(80, 40, 13, 20, image, red);
+	//}
+
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2;
+			int y0 = (v0.y + 1.) * height / 2;
+			int x1 = (v1.x + 1.) * width / 2;
+			int y1 = (v1.y + 1.) * height / 2;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
+
 	image.flip_vertically();//to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
+	delete model;
 	return 0;
 }
